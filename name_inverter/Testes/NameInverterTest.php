dd1fb408fe47a1a27877aeff03ad83a2aa05e31e
Franklyn <franklynkleiton@gmail.com>
2019-08-14 21:48:03 -0300

    Refactor step of cycle

--- name_inverter/Testes/NameInverterTest.php
+++ name_inverter/Testes/NameInverterTest.php
@@ -17,7 +17,7 @@ class NameInverterTest extends TestCase
     /**
      * @test
      */
-    public function givenEmptyString_returnEmptyString()
+    public function givenEmptyString_returnEmptyString(): void
     {
         $this->assertInverted("", "");
     }
@@ -25,7 +25,7 @@ class NameInverterTest extends TestCase
     /**
      * @test
      */
-    public function givenSimpleName_returnSimpleName()
+    public function givenSimpleName_returnSimpleName(): void
     {
         $this->assertInverted("Franklyn", "Franklyn");
     }
@@ -33,7 +33,7 @@ class NameInverterTest extends TestCase
     /**
      * @test
      */
-    public function givenNameWithSpace__returnSimpleName()
+    public function givenNameWithSpace__returnSimpleName(): void
     {
         $this->assertInverted(" Name ", "Name");
     }
@@ -41,7 +41,7 @@ class NameInverterTest extends TestCase
     /**
      * @test
      */
-    public function givenFirstLast_returnLastFirst()
+    public function givenFirstLast_returnLastFirst(): void
     {
         $this->assertInverted("First Last", "Last, First");
     }
@@ -49,7 +49,7 @@ class NameInverterTest extends TestCase
     /**
      * @test
      */
-    public function givenFirstLastNameWithSpaces_returnLastFirstNameWithoutSpaces()
+    public function givenFirstLastNameWithSpaces_returnLastFirstNameWithoutSpaces(): void
     {
         $this->assertInverted("  First  Last  ", "Last, First");
     }
@@ -57,7 +57,7 @@ class NameInverterTest extends TestCase
     /**
      * @test
      */
-    public function ignoreHonorifics()
+    public function ignoreHonorifics(): void
     {
         // Não viola a regra Triple A - Arrange, Act, Assert uma vez que os dois asserts garantem a veracidade do mesmo teste, lógicamente são um assert. Seria uma violação caso fosse feito um Arrange(Instâncias de váriaveis, objetos ou banco), Act(Função ex: invertName), Assert, Arrange, Act, Assert.
         $this->assertInverted("Mr. First Last", "Last, First");
@@ -67,11 +67,19 @@ class NameInverterTest extends TestCase
     /**
      * @test
      */
-    public function postNominal_stayInTheEnd()
+    public function postNominal_stayInTheEnd(): void
     {
         // Isso viola o single assert rule
         $this->assertInverted("First Last Sr.", "Last, First Sr.");
         $this->assertInverted("First Last BS. Phd.", "Last, First BS. Phd.");
+    }
+
+    /**
+     * @test
+     */
+    public function integration(): void
+    {
+        $this->assertInverted("Sr. Franklyn Kleiton II. King.", "Kleiton, Franklyn II. King.");
     }
 
     private function assertInverted($originalName, $invertedName)
@@ -81,45 +89,75 @@ class NameInverterTest extends TestCase
 
     private function invertName($name): string
     {
-        if ($name == null || strlen($name) <= 0) {
+        if ($name == null || strlen($name) <= 0)
             return "";
-        } else {
-            $names = $this->splitNames($name);
-            if (count($names) > 1 && $this->isHonorific($names)) {
-                array_splice($names, 0, 1);
-            }
-            if (count($names) == 1) {
-                return $names[0];
-            } else {
-                $postNominals = "";
-                if (count($names) > 2)
-                    $postNominals = $this->getPostNominals($names);
-                return trim($names[1].', '.$names[0].' '.$postNominals);
-            }
-        }
+        else
+            return $this->formatName($this->removeHonorifics($this->splitNames($name)));
     }
 
-    private function splitNames($names)
+    /**
+     * @param $names
+     * @return string
+     */
+    private function formatName($names): string
+    {
+        if (count($names) == 1)
+            return $names[0];
+        return $this->formatMultiElementName($names);
+    }
+
+    /**
+     * @param array $names
+     * @return array
+     */
+    private function removeHonorifics(array $names): array
+    {
+        if (count($names) > 1 && $this->isHonorific($names))
+            array_splice($names, 0, 1);
+        return $names;
+    }
+
+    private function splitNames($names): array
     {
         return preg_split('/\s+/', trim($names));
     }
 
-    private function isHonorific($word)
+    /**
+     * @param $names
+     * @return string
+     */
+    private function formatMultiElementName($names): string
+    {
+        $postNominals = "";
+        if (count($names) > 2)
+            $postNominals = $this->getPostNominals($names);
+        return trim($names[1] . ', ' . $names[0] . ' ' . $postNominals);
+    }
+
+    private function isHonorific($word): bool
     {
         return preg_match('/\./', $word[0]);
     }
 
-    private function getPostNominals($names)
-    {   $postNominals = [];
-        if (count($names) > 3) {
-            $postNominals = array_slice($names, 2, count($names)-1);
-        } else {
-            $postNominals = array_slice($names, 2);
-        }
+    private function getPostNominals($names): string
+    {
         $postNominal = "";
-        foreach ($postNominals as $nominal) {
-            $postNominal .= $nominal." ";
-        }
-        return $postNominal;       
+        foreach ($this->extractPostNominals($names) as $nominal)
+            $postNominal .= $nominal . " ";
+        return $postNominal;
+    }
+
+    /**
+     * @param $names
+     * @return array
+     */
+    private function extractPostNominals($names): array
+    {
+        $postNominals = [];
+        if (count($names) > 3)
+            $postNominals = array_slice($names, 2, count($names) - 1);
+        else
+            $postNominals = array_slice($names, 2);
+        return $postNominals;
     }
 }
